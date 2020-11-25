86ef9f1bb7327ad29003f5d1a43e67571004874c
AR-koki <teruuuuun72@gmail.com>
2020-11-25 03:56:49 +0000

    [Add]users_contoroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  namespace :admins do
+  end
+  devise_for :users
+  namespace :users do
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

877e96242b5f701241a600e575a7d7370d7ffa7f
AR-koki <teruuuuun72@gmail.com>
2020-11-25 05:13:45 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   devise_for :admins
   namespace :admins do
+  	et 'top' => 'admins#top', as:'top'
+  	resources :users, only: [:index, :edit, :show, :update, :unsubscribe]
+  	resources :movies
+  	resources :items
+  	resources :genres
   end
   devise_for :users
   namespace :users do
+  	root 'customers/homes#top'
+  	resources :users, only: [:index, :new, :create, :edit, :show, :update]
+  	resources :movies, only: [:index, :show]
+  	resources :items, only: [:index, :show]
+  	resources :genres, only: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

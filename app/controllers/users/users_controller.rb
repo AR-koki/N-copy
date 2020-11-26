fc504c069f73ff63ab5b6622d0e4d5fe8b86206d
AR-koki <teruuuuun72@gmail.com>
2020-11-26 21:01:10 +0000

    [Add]user_index

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,2 +1,9 @@
 class Users::UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :is_deleted)
+	end
 end

03a306f0b12165fbb17b7abd6bed915dd0304432
AR-koki <teruuuuun72@gmail.com>
2020-11-26 21:08:50 +0000

    [Add]user_controller

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,6 +2,20 @@ class Users::UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+	def new
+		@user = User.new
+	end
+	def create
+		@user = User.new(user_params)
+		@user.save
+		redirect_to users_movies_path
+	end
+	def show
+	end
+	def edit
+	end
+	def destory
+	end
 	private
 	def user_params
 		params.require(:user).permit(:name, :email, :is_deleted)

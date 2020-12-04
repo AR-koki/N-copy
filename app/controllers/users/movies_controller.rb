b64e9c3839b54563fa3e615bec14d64d5c3153f9
AR-koki <teruuuuun72@gmail.com>
2020-12-04 05:52:34 +0000

    [Add]user_movies.erb

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,6 +1,10 @@
 class Users::MoviesController < ApplicationController
   def index
   	@movies = Movie.where(is_active: true)
+  	@movie_houga = Movie.where(is_active: true).where(genre_id: 1)
+  	@movie_youga = Movie.where(is_active: true).where(genre_id: 2)
+  	@movie_anime = Movie.where(is_active: true).where(genre_id: 3)
+  	@movie_dorama = Movie.where(is_active: true).where(genre_id: 4)
   end
 
   def show

89247220e74984dce11e840da719ee33e6baf7d9
AR-koki <teruuuuun72@gmail.com>
2020-12-06 01:12:39 +0000

    [Add]model_MyList

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_063443) do
+ActiveRecord::Schema.define(version: 2020_12_06_011151) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 2020_11_29_063443) do
     t.integer "genre_id"
     t.text "introduction"
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "my_lists", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

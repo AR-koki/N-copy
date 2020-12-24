a0baec6a3feb4b76cb6d84b05cdc7f103bfed52d
AR-koki <teruuuuun72@gmail.com>
2020-12-24 03:28:47 +0000

    [Add]image_id

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,7 @@ class Movie < ApplicationRecord
 	belongs_to :genre, optional: true
 	has_many :my_lists, dependent: :destroy
 	mount_uploader :image_id, VideoUploader
+	mount_uploader :image_id, ImageIdUploader
 
 	def my_list_by?(user)
 		my_lists.where(user_id: user.id).exists?
